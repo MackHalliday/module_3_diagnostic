000607b74b9f60de16204e75c1dc9eafed6283cc
Alex Robinson <alexandra.scott335@gmail.com>
2019-10-09 17:12:04 -0600

    basic setup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  module ApplicationHelper
+  def hogwarts_houses
+    [
+      ['Gryffindor', 'Gryffindor'],
+      ['Ravenclaw', 'Ravenclaw'],
+      ['Slytherin', 'Slytherin'],
+      ['Hufflepuff', 'Hufflepuff'],
+    ]
+  end
+end
 end
