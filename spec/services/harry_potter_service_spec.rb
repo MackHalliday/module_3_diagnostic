5e1b591f51e3fe5b9139d77eee22dd02ddc34523
MackHalliday <mackenzie.halliday@gmail.com>
2019-10-14 09:44:18 -0600

    Finish testing setup for HarryPotterApiService:

--- spec/services/harry_potter_service_spec.rb
+++ spec/services/harry_potter_service_spec.rb
@@ -13,39 +13,9 @@ describe 'harry potter api service' do
     expect(service).to be_a(HarryPotterApiService)
     expect(raw_data).to be_a(Array)
 
-    expect(raw_data.first).to have_key(:name)
+    expect(raw_data.first).to have_key(:id)
     expect(raw_data.first).to have_key(:role)
     expect(raw_data.first).to have_key(:house)
     expect(raw_data.first).to have_key(:patronus)
-  end
-
-  it "github follower data", :vcr do
-
-    user = create(:user, github_token: ENV["GITHUB_API_KEY"])
-
-    service = GithubApiService.new
-
-    raw_data = service.get_follower_data(user)
-
-    expect(service).to be_a(GithubApiService)
-    expect(raw_data).to be_a(Array)
-
-    expect(raw_data.first).to have_key(:login)
-    expect(raw_data.first).to have_key(:html_url)
-  end
-
-  it "github following data", :vcr do
-
-    user = create(:user, github_token: ENV["GITHUB_API_KEY"])
-
-    service = GithubApiService.new
-
-    raw_data = service.get_following_data(user)
-
-    expect(service).to be_a(GithubApiService)
-    expect(raw_data).to be_a(Array)
-
-    expect(raw_data.first).to have_key(:login)
-    expect(raw_data.first).to have_key(:html_url)
   end
 end
